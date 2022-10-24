ac66f6f4f53e36839d454752d4483a79a340abe8
RenanSui <101724157+RenanSui@users.noreply.github.com>
2022-10-24 17:26:24 +0000

    Deploying to gh-pages from @ RenanSui/expense-tracker@1e357d4edad2013fc5b0c2f5928636f2deb82cfb 🚀

--- js/Show_Hide_Functions.js
+++ js/Show_Hide_Functions.js
@@ -3,8 +3,10 @@ const myForm = document.querySelector('#myForm');
 const detailsContainer = document.querySelector('#details');
 const myFormEdit = document.querySelector('#myForm-Edit');
 const modal = document.querySelector('#modal');
+const bodyElement = document.querySelector('#body');
 export const SHOW_HIDE_Form = (action) => {
     const form = myForm.classList;
+    const body = bodyElement.classList;
     if (!form.contains('hide') || !form.contains('show'))
         form.add('show');
     modal.classList.toggle('modal');
@@ -12,15 +14,18 @@ export const SHOW_HIDE_Form = (action) => {
         form.remove('hide');
         form.add('show');
         RESET_Form();
+        body.add('overflow-hidden');
     }
     else {
         form.remove('show');
         form.add('hide');
         RESET_Form();
+        body.remove('overflow-hidden');
     }
 };
 export const SHOW_HIDE_Details = (action) => {
     const form = detailsContainer.classList;
+    const body = bodyElement.classList;
     if (!form.contains('hide') || !form.contains('show'))
         form.add('show');
     modal.classList.toggle('modal');
@@ -28,15 +33,18 @@ export const SHOW_HIDE_Details = (action) => {
         form.remove('hide');
         form.add('show');
         RESET_Form();
+        body.add('overflow-hidden');
     }
     else {
         form.remove('show');
         form.add('hide');
         RESET_Form();
+        body.remove('overflow-hidden');
     }
 };
 export const SHOW_HIDE_Edit = (action) => {
     const form = myFormEdit.classList;
+    const body = bodyElement.classList;
     if (!form.contains('hide') || !form.contains('show'))
         form.add('show');
     modal.classList.toggle('modal');
@@ -44,10 +52,12 @@ export const SHOW_HIDE_Edit = (action) => {
         form.remove('hide');
         form.add('show');
         RESET_Form();
+        body.add('overflow-hidden');
     }
     else {
         form.remove('show');
         form.add('hide');
         RESET_Form();
+        body.remove('overflow-hidden');
     }
 };
